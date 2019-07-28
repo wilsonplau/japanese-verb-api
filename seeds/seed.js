8bd883030f984c28fcd6624ed2b9e284237c1da3
Wilson Lau <hello@wilsonplau.com>
2019-07-29 08:13:39 +0900

    Added seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,10 @@
 const { createVerbObject } = require('../classes/AllVerbs');
+const data = require('../data.js');
 
 exports.seed = function(knex) {
   return knex('verbs').del()
     .then(function () {
-      const allVerbs = [
-        {dictionary_form: '食べる', type: "ru-verb"},
-        {dictionary_form: '勉強する', type: "suru-verb"},
-        {dictionary_form: '帰る', type: "u-verb"},
-        {dictionary_form: '返す', type: "u-verb"}
-      ]
-      const verbsWithConjugations = allVerbs.map(({dictionary_form, type}) => {
+      const verbsWithConjugations = data.map(({dictionary_form, type}) => {
         return createVerbObject({dictionary_form, type}).all;
       });
       return knex('verbs').insert(verbsWithConjugations);
